84d7e863092b61ec9689c267f9672df4bad30421
Marcos Birro Calixto <58310513+marcosbirrocalixto@users.noreply.github.com>
2022-02-10 18:04:03 -0300

    0122 ok final

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <header-component></header-component>
+
+        <section>
+            <router-view></router-view>
+        </section>
+
+        <footer-component></footer-component>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from '@/layouts/components/HeaderComponent.vue'
+import FooterComponent from '@/layouts/components/FooterComponent.vue'
+
+export default {
+    name: 'DefaultTemplate',
+    components: {
+       HeaderComponent,
+       FooterComponent,
+    }
+}
+</script>
+
+<style scoped>
+@import url('../assets/css/plcmobile.css');
+@import url('../assets/css/reset.css');
+
+</style>
---
+++ src/views/supports/MySupports.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    Minhas dúvidas
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MySupports',
+}
+</script>
